9877592d17bfdf3e860841589235098be03c0252
ArefaEvgeniy <evgeniy_arefa@i.ua>
2023-07-22 21:44:05 +0300

    Added solution of lessons 7 and 8

--- lesson_07/home_1.py
+++ lesson_07/home_1.py
@@ -18,3 +18,48 @@
 #      и т.д.
 #  - использовать лямбда-функцию для того, что определить какое слово надо
 #    написать для конкретного числа: "раз" или "раза"
+
+
+from random import randint
+
+# Генерация случайного списка
+numbers = []
+for _ in range(200):
+    numbers.append(randint(1, 10))
+print("Первоначальный список: ", numbers)
+
+
+# Вариант 1
+def entries_count(sequence: list) -> dict:
+    """Функция подсчета кол-ва чисел в списке"""
+    result = {}
+    for item in sequence:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+
+    return result
+
+
+suffix_1 = lambda age: "раза" \
+    if (str(age)[-1] in "234" and (True if len(str(age)) == 1 else str(age)[-2] != '1')) \
+    else "раз"
+
+result = entries_count(numbers)
+for item in sorted(result):
+    print(f"Число {item} встречается в первоначальном списке "
+          f"{result[item]} {suffix_1(result[item])}")
+
+
+# Вариант 2
+print('-' * 50)
+
+suffix_2 = lambda age: "раза" \
+    if age % 10 in (2, 3, 4) and age % 100 not in (12, 13, 14) \
+    else "раз"
+
+result_2 = {x: numbers.count(x) for x in set(numbers)}
+for item in sorted(result_2):
+    print(f"Число {item} встречается в первоначальном списке "
+          f"{result_2[item]} {suffix_2(result_2[item])}")
