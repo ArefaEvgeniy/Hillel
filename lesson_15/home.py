f9167f82cc865c2cdec09617393dc8bbb2965dc3
ArefaEvgeniy <evgeniy_arefa@i.ua>
2023-08-08 19:29:58 +0300

    Added solutions of lessons 14, 15 and 16

--- lesson_15/home.py
+++ lesson_15/home.py
@@ -6,3 +6,41 @@
 # исключений, как минимум деление на 0.
 #
 # Создать своё исключение, к примеру возведение в отрицательную степень.
+
+
+class Negative_degree(Exception):
+
+    def __init__(self, message='Возведение в отрицательную степень'):
+        super().__init__(message)
+
+
+class Calculator(int):
+
+    def __init__(self, var):
+        self.var = var
+
+    def __add__(self, other):
+        try:
+            return Calculator(self.var + other)
+        except TypeError:
+            return 'Нельзя складывать цифры и буквы!'
+
+    def __sub__(self, other):
+        try:
+            return Calculator(self.var - other)
+        except TypeError:
+            return 'Нельзя от цифры отнять буквы!'
+
+    def __pow__(self, power, modulo=None):
+        if power >= 0:
+            return Calculator(self.var ** power)
+        else:
+            raise Negative_degree()
+
+    def __truediv__(self, other):
+        try:
+            return Calculator(self.var / other)
+        except ZeroDivisionError:
+            return 'Нельзя делить на ноль!'
+        except TypeError:
+            return 'Нельзя делить на буквы!'
