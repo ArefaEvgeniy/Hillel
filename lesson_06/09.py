5ae6d6343495db11edaeeec736605f76425019ee
ArefaEvgeniy <evgeniy_arefa@i.ua>
2023-09-08 09:46:59 +0300

    Added lesson 7

--- lesson_06/09.py
+++ lesson_06/09.py
@@ -1,14 +1,19 @@
 name = 'Tom'
 
 
-def say_hi():
+def say_hi(name):
     def say_bye():
+        global name
+        name = 'Kat'
         print('Bye,', name)
 
+    name = 'Jack'
     print('Hello,', name)
     say_bye()
+    print('How are you,', name)
+    return name
 
 
-say_hi()
+name = say_hi(name)
 print(name)
-say_bye()
+# say_bye()
