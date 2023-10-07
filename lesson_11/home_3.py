74790cda5016128836c3d4d6f59041ed421a2b86
ArefaEvgeniy <evgeniy_arefa@i.ua>
2023-10-07 19:40:07 +0300

    Added solution of lessons 10 and 11

--- lesson_11/home_3.py
+++ lesson_11/home_3.py
@@ -6,3 +6,49 @@
 # змісту підсумкового файлу.
 
 
+import csv
+import openpyxl
+
+input_data = []
+names = None
+
+# відкриття файлу для читання
+with open("file_csv.csv", encoding='utf-8') as r_file:
+    file_reader = csv.reader(r_file, delimiter=",")
+    count = 0
+    for row in file_reader:
+        if count == 0:
+            names = row  # вичитування найменувань стовпців (перший рядок у файлі)
+        else:
+            input_data.append(row)  # зчитування даних рядково
+        count += 1
+
+# знаходження індексу стовпця 'age'
+age_index = [index for index, item in enumerate(names) if item == 'age']
+age_index = age_index[0] if age_index else None
+
+# якщо цей стовпець є, то видалити його значення зі списку найменувань і зі списку даних
+if age_index is not None:
+    names.pop(age_index)
+    for item_list in input_data:
+        item_list.pop(age_index)
+
+wb = openpyxl.Workbook()  # відкриття книги excel
+sheet = wb.active  # вибір активного листа
+
+# Запис верхнього рядка person 1, person 2 ... починаючи з другого стовпця
+# Кількість записів person дорівнює кількості списків всередині input_data
+for item in range(1, len(input_data) + 1):
+    cell = sheet.cell(row=1, column=item + 1)
+    cell.value = f'person {item}'
+
+# цикл перебору рядків
+for name_index, name in enumerate(names):
+    cell = sheet.cell(row=name_index+2, column=1)
+    cell.value = name
+    # цикл перебору стовпців
+    for item in range(1, len(input_data) + 1):
+        cell = sheet.cell(row=name_index+2, column=item + 1)
+        cell.value = input_data[item - 1][name_index]
+
+wb.save('file_xlsx.xlsx')
--- lesson_13/home_2.py
+++ lesson_13/home_2.py
@@ -9,13 +9,13 @@
 # потім відбувається конкаткатенація. Результатом додавання буде новий об'єкт
 # класу String. Приклади виконання:
 #
-# String('New') + String(890)    ->    'New890'
-# String(1234) + 5678            ->    '12345678'
-# String('New') + 'castle'       ->    'Newcastle'
-# String('New') + 77             ->    'New77'
-# String('New') + True           ->    'NewTrue'
-# String('New') + ['s', ' ', 23] ->    "New['s', ' ', 23]"
-# String('New') + None           ->    'NewNone'
+# print(String('New') + String(890))    ->    'New890'
+# print(String(1234) + 5678)            ->    '12345678'
+# print(String('New') + 'castle')       ->    'Newcastle'
+# print(String('New') + 77)             ->    'New77'
+# print(String('New') + True)           ->    'NewTrue'
+# print(String('New') + ['s', ' ', 23]) ->    "New['s', ' ', 23]"
+# print(String('New') + None)           ->    'NewNone'
 #
 # Принцип віднімання в новому класі String: з об'єкту типу String ви можете
 # відняти значення будь-якого іншого типу, яке можна привести до типу рядка.
@@ -25,12 +25,12 @@
 # першому операнді немає значення другого операнду, то результатом віднімання
 # стане перший операнд без змін. Приклади виконання:
 #
-# String('New bala7nce') - 7               ->    'New balance'
-# String('New balance') - 'bal'            ->    'New ance'
-# String('New balance') - 'Bal'            ->    'New balance'
-# String('pineapple apple pine') - 'apple' ->    'pine apple pine'
-# String('New balance') - 'apple'          ->    'New balance'
-# String('NoneType') - None                ->    'Type'
-# String(55678345672) - 7                  ->    '5568345672'
+# print(String('New bala7nce') - 7)               ->    'New balance'
+# print(String('New balance') - 'bal')            ->    'New ance'
+# print(String('New balance') - 'Bal')            ->    'New balance'
+# print(String('pineapple apple pine') - 'apple') ->    'pine apple pine'
+# print(String('New balance') - 'apple')          ->    'New balance'
+# print(String('NoneType') - None)                ->    'Type'
+# print(String(55678345672) - 7)                  ->    '5568345672'
 #
 # *Важливо! Результатом додавання або віднімання завжди буде об'єкт типу String
