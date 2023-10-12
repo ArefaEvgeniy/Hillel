c0b18db1ce6ffc177682a6b9af082bd27e047773
ArefaEvgeniy <evgeniy_arefa@i.ua>
2023-10-12 21:29:09 +0300

    Added lesson 17

---
+++ lesson_17/task_01.py
@@ -0,0 +1,12 @@
+def summa(a, b):
+    try:
+        return a + b
+    except TypeError:
+        print('Not supported type')
+    except Exception:
+        print('Something is wrong')
+    return
+
+
+def mines(a, b):
+    return a - b
