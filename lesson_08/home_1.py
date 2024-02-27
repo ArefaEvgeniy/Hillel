f473f4612148b2dd31049240b19c64b25a18dfad
evgeniy <areinna@i.ua>
2024-02-27 21:25:09 +0200

    Added solution os lessons 7 and 8

--- lesson_08/home_1.py
+++ lesson_08/home_1.py
@@ -6,3 +6,7 @@
 
 
 values = [1, 2, '3', 'forth', 'end', 99, True, None]
+
+new_value = list(map(lambda x: str(x) if isinstance(x, int) and not isinstance(x, bool) else x, values))
+
+print(new_value)
