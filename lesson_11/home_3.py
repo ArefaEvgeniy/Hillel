c2978fb1bba7ca29223e8fd2e0cb385cb348a683
evgeniy <areinna@i.ua>
2024-03-09 11:55:07 +0200

    Added solutions of lessons 9, 10 and 11

--- lesson_11/home_3.py
+++ lesson_11/home_3.py
@@ -4,3 +4,26 @@
 # розгорнути таблицю на дев'яносто градусів (стовпці стають рядками, а рядки
 # стовпцями). До завдання прикріплений приклад як має виглядати змісту
 # підсумкового файлу.
+
+import openpyxl
+import csv
+
+
+with open('data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    rows = list(csv_reader)
+    num_rows = sum(1 for row in csv_reader)
+
+wb = openpyxl.Workbook()
+ws = wb.active
+
+for r, row in enumerate(rows, start=1):
+    ws.cell(row=1, column=1, value='')
+    ws.cell(row=1, column=r, value='Person ' + str(r - 1))
+    for c, val in enumerate(row, start=2):
+        if c == 4:
+            continue
+        ws.cell(row=c if c < 4 else c - 1, column=r, value=val)
+
+wb.save('file_xlsx.xlsx')
+wb.close()
