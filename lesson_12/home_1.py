965252f14435a1f2e02629ab5dce24bdfcd3efcb
evgeniy <areinna@i.ua>
2024-03-12 21:20:43 +0200

    Added solution of lesson 12

--- lesson_12/home_1.py
+++ lesson_12/home_1.py
@@ -4,3 +4,21 @@
 # Методи move і stop виводять повідомлення на екран «move» та «stop», а
 # birthday збільшує атрибут age на 1.
 # Атрибути brand, age та mark є обов'язковими під час оголошення об'єкта.
+
+class Auto(object):
+
+    def __init__(self, brand, age, mark, color=None, weight=None):
+        self.brand = brand
+        self.age = age
+        self.color = color
+        self.mark = mark
+        self.weight = weight
+
+    def move(self):
+        print("move")
+
+    def birthday(self):
+        self.age += 1
+
+    def stop(self):
+        print("stop")
