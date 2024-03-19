94a0f23ef6c6cff8f433f227997ef03873588b0b
evgeniy <areinna@i.ua>
2024-03-19 21:00:09 +0200

    Added solution of lesson 16

--- lesson_16/home.py
+++ lesson_16/home.py
@@ -17,3 +17,21 @@
 # 810
 # 2430
 # ...
+
+def simple_generator(start, step):
+    val = start
+    while True:
+        yield val
+        val *= step
+
+
+for item in simple_generator(-2, -5):
+    print(item)
+    if -1000 <= item >= 1000:
+        break
+
+
+for item in simple_generator(10, 3):
+    print(item)
+    if -1000 <= item >= 1000:
+        break
