fde8c7c7a981ebea78e4eeef435c9e68f3ac3070
evgeniy <areinna@i.ua>
2024-05-07 22:31:29 +0300

    Added solutions for lessons 11 and 12

--- lesson_11/home_1.py
+++ lesson_11/home_1.py
@@ -6,3 +6,6 @@
 import json
 data = {111111: ('Sam', 42), 100003: ('Tom', 12), 222043: ('Adam', 69),
         679432: ('Jasy', 25), 980451: ('Kim', 33), 666740: ('Jasy', 15)}
+
+with open('file_json.json', 'w') as file_json:
+    json.dump(data, file_json)
