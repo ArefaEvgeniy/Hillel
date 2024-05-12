e681afd71907bf4140e6f48dc4dc226a3f258ad9
evgeniy <areinna@i.ua>
2024-05-12 21:16:52 +0300

    Added solution for lessons 13 and 14

--- Add_task/28.py
+++ Add_task/28.py
@@ -17,3 +17,14 @@
 # 34
 
 
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+N = int(input("Введите число: "))
+fib_gen = fibonacci_generator()
+for i in range(N):
+    print(next(fib_gen))
