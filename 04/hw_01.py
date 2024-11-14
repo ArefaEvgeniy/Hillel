cf3ce5837844220d38cceb1498426230643f6933
evgeniy <areinna@i.ua>
2024-11-14 21:22:10 +0200

    Added solution for lesson 04

--- 04/hw_01.py
+++ 04/hw_01.py
@@ -10,3 +10,14 @@
 #
 # Для перевірки коректності роботи Вашого коду використовуйте приклади вище.
 # Робити запит на введення даних від користувача не потрібно.
+
+
+input_list = [1, 0, 13, 0, 0, 0, 5]
+print("Origin list: ", input_list)
+
+zero_qty = input_list.count(0)
+while zero_qty > 0:
+    input_list.remove(0)
+    input_list.append(0)
+    zero_qty -= 1
+print("Modified list: ", input_list)
