15959cddda23a5a95012ffa90957699ec5bbd3fc
evgeniy <areinna@i.ua>
2025-01-23 19:53:28 +0200

    Second commit

--- 01/1.py
+++ 01/1.py
@@ -3,9 +3,12 @@ import requests
 from math import pi
 
 
-print("Hellow, world")
+print("Hellow, worlds")
 print(pi)
 
 response = requests.get('https://shop.flixbus.ua/')
 print(response.status_code)
 print(response.text)
+
+a = 90
+print(a)
