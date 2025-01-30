edda44622dbea00da6257d5d631c7e9586e99bf8
evgeniy <areinna@i.ua>
2025-01-30 21:23:09 +0200

    Added lesson 03

---
+++ 03/01.py
@@ -0,0 +1,20 @@
+a = 10
+b = 10.0
+c = 10
+d = 10000
+
+print(a == b)
+print(a != 11)
+print(a != b)
+print(a > b)
+print(a < 11)
+print(a >= b)
+print(a <= b)
+
+print(a is b)
+print(a is c)
+
+print("id(a):", id(a))
+print("id(b):", id(b))
+print("id(c):", id(c))
+print("id(d):", id(d))
---
+++ 03/02.py
@@ -0,0 +1,17 @@
+print(bool(23))
+print(bool(-23))
+print(bool(23.88))
+print(bool(0))
+print(bool('Hello'))
+print(bool('H'))
+print(bool(' '))
+print(bool(''))
+
+print(int(True))
+print(int(False))
+
+
+a = None
+
+print(a)
+print(type(a))
