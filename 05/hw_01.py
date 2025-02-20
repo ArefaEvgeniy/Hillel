74c90c98ae815e437462ada4b0030fd23ba1549a
evgeniy <areinna@i.ua>
2025-02-20 21:33:05 +0200

    Added solutions lesson 04

--- 05/hw_01.py
+++ 05/hw_01.py
@@ -12,3 +12,22 @@
 # 'Python Community' -> #PythonCommunity
 # 'i like python community!' -> #ILikePythonCommunity
 # 'Should, I. subscribe? Yes!' -> #ShouldISubscribeYes
+
+
+import string
+import keyword
+
+
+result = True
+name = input("Введіть рядок: ")
+if len(name) > 1 and all(char == '_' for char in name):
+    result = False
+if name[0].isdigit():
+    result = False
+if any(char.isupper() or char in string.whitespace or 
+       char in string.punctuation.replace('_', '') for char in name):
+    result = False
+if name in keyword.kwlist:
+    result = False
+
+print(result)
