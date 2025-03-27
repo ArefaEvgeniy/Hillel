6730b1b6c7a11ea4b0443739c08f7f842d678058
evgeniy <areinna@i.ua>
2025-03-27 21:02:56 +0200

    Added lesson 16

---
+++ 16/09.py
@@ -0,0 +1,9 @@
+#  1) Web
+#    Flask, Django, Pillow, HTML, CSS, Java-Script, SQL, MySQL, Postgres, MariaDB
+#  2) Big DATA
+#    Pandas, NunPy, Pillow, SQL, MySQL, Postgres, MariaDB
+#  3) AI
+#  4) Base, Scrapy, ButifulSoup, Selenium, PyGame ...
+#  5) Auto QA, Selenium, ButifulSoup
+
+#  re, sys, os, os.path, datetime, time
