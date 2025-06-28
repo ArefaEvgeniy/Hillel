b34cd64cc055da64eca892fd0b571b56e8f256a4
evgeniy <areinna@i.ua>
2025-06-28 23:36:16 +0300

    Homework Solutions for lesson 06

--- 06/hw_03.py
+++ 06/hw_03.py
@@ -10,3 +10,13 @@
 # 25 -> 0
 # 1 -> 1
 
+
+user_number = int(input('Enter your integer number: '))
+
+while user_number > 9:
+    result = 1
+    for i in str(user_number):
+        result *= int(i)
+    user_number = result
+
+print(user_number)
