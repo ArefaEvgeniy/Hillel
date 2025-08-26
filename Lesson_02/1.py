a6b762787ede1c9073a2fe3c1ffa965630978c63
evgeniy <evgeniy@google.com>
2025-08-26 21:25:07 +0300

    Added Lesson Bonus

---
+++ Lesson_02/1.py
@@ -0,0 +1,16 @@
+a = 33  # age
+b = "Bob"
+
+# Rewrite the code using more descriptive variable names
+age = 33
+name = "Bob"
+
+cell_phone_number = "123-456-7890"
+cellPhoneNumber = "123-456-7890"
+
+# PEP-8
+print('Hello, World!')
+
+print = 44
+
+# print('Hello, World!')
