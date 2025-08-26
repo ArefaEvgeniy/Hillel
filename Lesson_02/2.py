e1682375e48b346c5c3b2cf9e9c697ba11826ffc
evgeniy <evgeniy@google.com>
2025-08-26 21:25:07 +0300

    Added Lesson Bonus

---
+++ Lesson_02/2.py
@@ -0,0 +1,23 @@
+a = 3
+b = 3.0
+c = 3.99
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+d = int(b)
+e = int(c)
+f = float(a)
+
+print(d)
+print(e)
+print(f)
+print(type(d))
+print(type(e))
+print(type(f))
+
+# "A" -> 6 -> 101
+
+# 0B10 011 101 000 101 -> 0H24606
+# 0B10 0111 0100 0101 -> 0X2745
