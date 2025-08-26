9cb03bd2a867d20bb0c127f1658b9dc0ff3568ea
evgeniy <evgeniy@google.com>
2025-08-26 19:51:25 +0300

    first commit

---
+++ Lesson_01/2.py
@@ -0,0 +1,12 @@
+# import langdetect
+from langdetect import detect
+
+
+# lang1 = detect("War doesn't show who's right, just who's left.")
+# lang2 = detect("Ein, zwei, drei, vier")
+#
+# print(lang1)
+# print(lang2)
+
+print(detect("War doesn't show who's right, just who's left."))
+print(detect("Ein, zwei, drei, vier"))
