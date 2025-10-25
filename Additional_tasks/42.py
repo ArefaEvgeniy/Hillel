87d2ce2eafc25959c33341a1c5829b6bf1558858
evgeniy <evgeniy@google.com>
2025-10-25 13:23:48 +0300

    Added solutions for additional tasks 41-44

--- Additional_tasks/42.py
+++ Additional_tasks/42.py
@@ -3,3 +3,14 @@
 # починаються на "q" або "f" виключені. Використовувати ланцюжки.
 # Приклад: ["The", "quick", "brown", "fox"] -> ["THE", "brown"]
 
+
+def funk2(words_list):
+    return map(
+        lambda x: x.upper() if len(x) == 3 else x, 
+        filter(lambda x: not x.startswith(('q', 'f')), words_list)
+    )
+
+
+words = ["The", "quick", "brown", "fox"]
+
+print(list(funk2(words)))
