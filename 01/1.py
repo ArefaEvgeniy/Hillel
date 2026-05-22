160c420adbea9c9922ff4e6daa834634f7a6a51a
evgeniy <evgeniy@google.com>
2026-05-22 20:36:49 +0300

    first commit

---
+++ 01/1.py
@@ -0,0 +1,20 @@
+import sys
+import math
+from math import pi
+
+
+name = "Bob"
+print('Hello, world')
+print()
+print("Hello,", name)
+name = "Irina"
+print("Hello,", name)
+
+print("Hello,", sys.argv[1])
+
+print("Pi =", math.pi)
+print("Pi =", pi)
+
+# data = input("Enter your name: ")
+#
+# print("Hello,", data)
---
+++ 01/2.py
@@ -0,0 +1,16 @@
+from langdetect import detect
+# from xxxxx import detect as X_detect
+# import langdetect
+# import xxxxx
+
+
+print(detect("War doesn't show who's right, just who's left."))
+# print(X_detect("Ein, zwei, drei, vier"))
+
+# print = 10
+#
+# a = 20 + print
+#
+# print(a)
+
+# PEP-8
