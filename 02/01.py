ecd8d22f8c302728bcb16903a28324065f8bb990
evgeniy <evgeniy@google.com>
2026-05-22 21:48:13 +0300

    Lesson 02

---
+++ 02/01.py
@@ -0,0 +1,19 @@
+a = 10.66
+name = "Nick"
+max_age = 100
+
+print("prise is", a)
+
+c = 100 - a
+
+print("prise is", c)
+
+print(id(max_age))
+print(id(name))
+
+b = 100
+print(id(b))
+
+b = b + 2
+print(id(b))
+
---
+++ 02/02.py
@@ -0,0 +1,14 @@
+a = -10
+b = 0
+c = 10
+d = 100
+e = -10000
+f = 12908
+
+print(id(a))
+print(id(b))
+print(id(c))
+print(id(d))
+print(id(e))
+print(id(f))
+print(id(12.12))
