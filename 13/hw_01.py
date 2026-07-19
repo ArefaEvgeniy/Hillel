8161765ec0d4d16efe3c52fb62e11ed01ed2d6e1
evgeniy <evgeniy@google.com>
2026-07-19 16:44:56 +0300

    Added solution to home work of Lesson 13

--- 13/hw_01.py
+++ 13/hw_01.py
@@ -1,13 +1,137 @@
-# Ваше завдання написати функцію, яка прочитає заданий файл, очистить текст
-# від html-тегів і запише цей текст в інший файл. html-тег завжди починається
-# з "<" і закінчується на ">" тобто. потрібно видалити ці символи та все, що
-# між ними. Функція отримує на вхід два параметри – ім'я файлу, який потрібно
-# очистити, та ім'я файлу, куди потрібно записати очищений текст. Ім'я файлу,
-# куди потрібно писати, можна задати за замовчуванням.
-# Приклади файлів у вкладенні – файл який потрібно очистити (draft.html) та
-# приклад файлу, який може вийти на виході з очищеним текстом (cleaned.txt).
-# Файл draft.html необхідно скачати і покласти поряд з файлом, де буде
-# вирішення цієї домашки.
+# Створіть клас, що описує людину (створіть метод, що виводить інформацію про
+# людину). На його основі створіть клас Студент (перевизначте метод виведення
+# інформації). Створіть клас Група, екземпляр якого складається з об'єктів
+# класу Студент. Реалізуйте методи додавання, видалення студента та метод
+# пошуку студента на прізвище. Метод пошуку студента повинен повертати саме
+# екземпляр класу Студент, якщо студент є у групі, інакше - None.
 #
-# Додаткове завдання для тих, хто захоче ускладнити рішення - спробуйте
-# прибрати рядки, в яких немає інформації.
+# У методі видалення, використовуйте результат методу пошуку.
+# Тобто, потрібно скомбінувати ці два методи;)
+# Визначте для групи метод str() для повернення списку студентів у вигляді рядка.
+#
+# Нижче наведені заготовки, які необхідно доповнити.
+
+# class Human:
+#
+#     def __init__(self, gender, age, first_name, last_name):
+#         pass
+#
+#     def __str__(self):
+#         pass
+#
+# class Student(Human):
+#
+#     def __init__(self, gender, age, first_name, last_name, record_book):
+#         pass
+#
+#     def __str__(self):
+#         pass
+#
+# class Group:
+#
+#     def __init__(self, number):
+#         self.number = number
+#         self.group = set()
+#
+#     def add_student(self, student):
+#         self.group.add(student)
+#
+#     def delete_student(self, last_name):
+#         pass
+#
+#     def find_student(self, last_name):
+#         pass
+#
+#     def __str__(self):
+#         all_students = ''
+#         ...
+#         return f'Number:{self.number}\\n {all_students} '
+#
+# st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+# st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+# gr = Group('PD1')
+# gr.add_student(st1)
+# gr.add_student(st2)
+# print(gr)
+# assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+# assert gr.find_student('Jobs2') is None, 'Test2'
+# assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+#
+# gr.delete_student('Taylor')
+# print(gr)  # Only one student
+#
+# gr.delete_student('Taylor')  # No error!
+
+
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"Person: {self.first_name} {self.last_name}.\nGender: {self.gender}\nAge: {self.age}"
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{super().__str__()}, Record book number: {self.record_book}"
+
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+        else:
+            return 'No, error!'
+
+    def find_student(self, last_name):
+        res = None
+        for student in self.group:
+            if student.last_name == last_name:
+                res = student
+                break
+        return res
+
+    def __str__(self):
+        all_students = ';\n'.join([str(student) for student in gr.group])
+        return f'Number:{self.number}\n{all_students} '
+
+
+def my_func():
+    print('My func')
+
+
+if __name__ == "__main__":
+    st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+    st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+    gr = Group('PD1')
+    gr.add_student(st1)
+    gr.add_student(st2)
+
+    print(gr)
+
+    assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+    assert gr.find_student('Jobs2') is None, 'Test2'
+    assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+
+    gr.delete_student('Taylor')
+
+    print(gr)  # Only one student
+
+    print(gr.delete_student('Taylor'))  # No error!
--- 13/hw_02.py
+++ 13/hw_02.py
@@ -1,76 +1,127 @@
-# Створіть клас для опису товару (припустимо, це заділ для інтернет-магазину).
-# Як поля товару можете використовувати значення ціни, опис, габарити товару.
-# Створіть пару екземплярів вашого класу та протестуйте їхню роботу. Створіть
-# клас "Покупець". Як поля можна використовувати прізвище, ім'я, по батькові,
-# мобільний телефон і т.д. Створіть клас "Замовлення". Замовлення може містити
-# кілька товарів, причому кількість кожного з товарів може бути різною.
-# Замовлення має бути "підв'язане" до користувача, який його здійснив.
-# Реалізуйте метод обчислення сумарної вартості замовлення. Визначте метод
-# str() для коректного виведення інформації про це замовлення.
+# Створити клас цифрового лічильника. У класі реалізувати методи:
+# - встановлення максимального значення лічильника,
+# - встановлення мінімального значення лічильника
+# - встановлення початкового значення лічильника
+# - метод step_up збільшує лічильник на 1. Метод можна викликати до тих пір,
+#   поки значення досягне максимуму. При досягненні максимуму слід викинути
+#   (raise) виняток ValueError, з описом, що досягнуто максимумуʼ
+# - метод step_down зменшує лічильник на 1. Метод можна викликати до тих пір,
+#   поки значення не досягне мінімуму. При досягненні мінімуму потрібно
+#   викинути (raise) виняток ValueError, з описом, що досягнутий мінімум
+# - повернення поточного значення лічильника
+# Початкове, мінімальне та максимальне значення лічильника також можуть бути
+# додані в метод ініціалізації екземпляра класу.
 #
-# class Item:
+# Приблизний каркас для класу та варіанти перевірки. Вам потрібно дописати
+# необхідне замість pass
+
+# class Counter:
 #
-#     def __init__(self, name, price, description, dimensions):
-#         self.price = price
-#         self.description = description
-#         self.dimensions = dimensions
-#         self.name = name
+#    def __init__(self, current=1, min_value=0, max_value=10):
+#        self.current = current
+#        self.min_value = min_value
+#        self.max_value = max_value
 #
-#     def __str__(self):
+#    def set_current(self, start):
+#        self.current = start
+#
+#    def set_max(self, max_max):
 #         pass
 #
-# class User:
+#    def set_min(self, min_min):
+#        pass
 #
-#     def __init__(self, name, surname, numberphone):
-#         self.name = name
-#         self.surname = surname
-#         self.numberphone = numberphone
+#    def step_up(self):
+#        pass
 #
-#     def __str__(self):
-#         pass
+#    def step_down(self):
+#        pass
 #
-# class Purchase:
-#     def __init__(self, user):
-#         self.products = {}
-#         self.user = user
-#         self.total = 0
+#    def get_current(self):
+#        return self.current
 #
-#     def add_item(self, item, cnt):
-#         self.products[item] = cnt
+# counter = Counter()
+# counter.set_current(7)
+# counter.step_up()
+# counter.step_up()
+# counter.step_up()
+# assert counter.get_current() == 10, 'Test1'
+# try:
+#     counter.step_up()  # ValueError
+# except ValueError as e:
+#     print(e) # Достигнут максимум
+# assert counter.get_current() == 10, 'Test2'
 #
-#     def __str__(self):
-#         pass
-#
-#     def get_total(self):
-#         pass
-#
-# lemon = Item('lemon', 5, "yellow", "small", )
-# apple = Item('apple', 2, "red", "middle", )
-# print(lemon)  # lemon, price: 5
-#
-# buyer = User("Ivan", "Ivanov", "02628162")
-# print(buyer)  # Ivan Ivanov
-#
-# cart = Purchase(buyer)
-# cart.add_item(lemon, 4)
-# cart.add_item(apple, 20)
-# print(cart)
-# """
-# User: Ivan Ivanov
-# Items:
-# lemon: 4 pcs.
-# apple: 20 pcs.
-# """
-# assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
-# assert cart.get_total() == 60, "Всього 60"
-# assert cart.get_total() == 60, 'Повинно залишатися 60!'
-# cart.add_item(apple, 10)
-# print(cart)
-# """
-# User: Ivan Ivanov
-# Items:
-# lemon: 4 pcs.
-# apple: 10 pcs.
-# """
-#
-# assert cart.get_total() == 40
+# counter.set_min(7)
+# counter.step_down()
+# counter.step_down()
+# counter.step_down()
+# assert counter.get_current() == 7, 'Test3'
+# try:
+#     counter.step_down()  # ValueError
+# except ValueError as e:
+#     print(e) # Достигнут минимум
+# assert counter.get_current() == 7, 'Test4'
+
+
+class Counter:
+
+    def __init__(self, current=1, min_value=0, max_value=10):
+        if min_value > max_value:
+            raise ValueError("Мінімальне значення не може бути більше максимального")
+        self.min_value = min_value
+        self.max_value = max_value
+        self.current = min_value
+        self.set_current(current)
+
+    def set_current(self, current):
+        if not self.min_value <= current <= self.max_value:
+            raise ValueError("Нове поточне значення знаходиться поза діапазоном між мінімальним та максимальним")
+        self.current = current
+
+    def set_max(self, max_value):
+        if max_value < self.current:
+            raise ValueError("Нове максимальне значення менше поточного")
+        self.max_value = max_value
+
+    def set_min(self, min_value):
+        if min_value > self.current:
+            raise ValueError("Нове мінімальне значення більше поточного")
+        self.min_value = min_value
+
+    def step_up(self):
+        if self.current + 1 > self.max_value:
+            raise ValueError('Досягнуто максимуму')
+        self.current += 1
+
+    def step_down(self):
+        if self.current - 1 < self.min_value:
+            raise ValueError('Досягнуто мінімуму')
+        self.current -= 1
+
+    def get_current(self):
+        return self.current
+
+
+counter = Counter()
+counter.set_current(7)
+counter.step_up()
+counter.step_up()
+counter.step_up()
+assert counter.get_current() == 10, 'Test1'
+try:
+    counter.step_up()  # ValueError
+except ValueError as e:
+    print(e)  # Досягнуто максимуму
+assert counter.get_current() == 10, 'Test2'
+
+counter.set_min(7)
+counter.step_down()
+counter.step_down()
+counter.step_down()
+assert counter.get_current() == 7, 'Test3'
+try:
+    counter.step_down()  # ValueError
+except ValueError as e:
+    print(e)  # Досягнуто мінімуму
+assert counter.get_current() == 7, 'Test4'
